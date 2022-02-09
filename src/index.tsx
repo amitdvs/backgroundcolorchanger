c0c60c636c517d4d9109610a87766ea63febcb8b
Amit Singh Rawat <amit.rawat@dvsoftech.com>
2022-02-09 17:25:18 +0530

    code update

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,9 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root");
+
 if (!container) throw new Error("Failed to find the root element");
+
 const root = ReactDOM.createRoot(container);
 
 root.render(
